3309d098980eb49b01bb9ab15bc0c5de0af77a89
GBN6 <czesiox8@gmail.com>
2022-10-10 18:09:07 +0200

    Add product filter based on category

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,9 +4,12 @@ import ProductCard from '../components/ProductCard';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [filtered, setFiltered] = useState(products);
 
   useEffect(() => {
     fetchProducts();
+    handleFilterCategory();
   }, []);
 
   const fetchProducts = async () => {
@@ -14,19 +17,50 @@ const Shop = () => {
     let data = await productsResponse.json();
     console.log(data);
     setProducts(data);
+    const category = [...new Set(data.map((x) => x.category.toUpperCase()))];
+    setCategories(category);
   };
-  const category = [...new Set(products.map((x) => x.category.toUpperCase()))];
 
-  const productCards = products.map((product) => (
-    <ProductCard key={product.id} image={product.image} price={product.price} title={products.title} />
+  const productCards = filtered.map((product) => (
+    <ProductCard
+      key={product.id}
+      image={product.image}
+      price={product.price}
+      title={product.title}
+    />
   ));
+
+  const handleFilterCategory = (category) => {
+    const productsCopy = [...products];
+    if (!category) {
+      setFiltered(productsCopy);
+    } else if (category === 'jewelery') {
+      setFiltered(productsCopy.filter((item) => item.category === category));
+    } else if (category === 'electronics') {
+      setFiltered(productsCopy.filter((item) => item.category === category));
+    } else if (category === "women's clothing") {
+      setFiltered(productsCopy.filter((item) => item.category === category));
+    } else if (category === "men's clothing") {
+      setFiltered(productsCopy.filter((item) => item.category === category));
+    }
+  };
+
+  const handleCategory = (e) => {
+    const categoryTitle = document.querySelector('.product-category-name');
+    const category = e.target.textContent.toLowerCase();
+    categoryTitle.textContent = category.toUpperCase();
+    handleFilterCategory(category);
+  };
 
   return (
     <div className='products-container'>
       <div className='category-container'>
+        <h2 className='product-category-name'>All products</h2>
         <ul>
-          {category.map((item) => (
-            <li key={item}>{item}</li>
+          {categories.map((item) => (
+            <li onClick={handleCategory} key={item}>
+              {item}
+            </li>
           ))}
         </ul>
       </div>
