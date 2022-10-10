931f0b92dd84b3f06b6a53f5c24b32591ec5eb89
GBN6 <czesiox8@gmail.com>
2022-10-10 19:14:55 +0200

    Fix cart quantity

--- src/App.js
+++ src/App.js
@@ -6,26 +6,34 @@ import Footer from './components/Footer';
 import Shop from './pages/Shop';
 
 const App = () => {
-
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (product) => {
-    console.log(cart)
+    console.log(cart);
     let isInCart = false;
-
+    if (cart.length > 0) {
+      cart.forEach((item) => {
+        if (item.product.title === product.title) {
+          item.product.quantity += 1;
+          isInCart = true;
+        }
+      });
+    }
     if (!isInCart) {
-      setCart((prevState) => [...prevState, {product}])
+      setCart((prevState) => [...prevState, { product }]);
       isInCart = true;
     }
-    
-  } 
+  };
 
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop handleAddToCart={handleAddToCart}/>} />
+        <Route
+          path='/shop'
+          element={<Shop handleAddToCart={handleAddToCart} />}
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
